94e750948180b5d23bbfc901ba70feeaaaee315b
Lucas Fernandes <lucas.adsf2@gmail.com>
2022-01-04 19:10:26 -0300

    Removido os comentários do fonte.

--- estrutura-controle.js
+++ estrutura-controle.js
@@ -1,16 +1,3 @@
-/*escreval("Digite o Nome do Aluno")
-   leia(nome)
-   escreval("Digite a nota 01 do aluno: ")
-   leia(nota01)
-   escreval("Digite a nota 02 do aluno: ")
-   leia(nota02)
-   media := (nota01 + nota02) / 2
-
-   se media >= 5 entao
-      escreval(nome, " Aprovado!")
-   senao
-        escreval(nome, " Reprovado!")
-   fimse*/
    var nome, nota01, nota02, media;
    nome = prompt("Digite o nome do aluno: ")
    nota01 = prompt("Digite a nota 01 do aluno: ")
